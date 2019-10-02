1661a37e660f25c8992aa55027da04e7a23349ea
Albersheim <James.Albersheim@thomsonreuters.com>
2019-10-02 18:15:25 -0500

    use albersheim

--- src/main/java/com/albersheim/service/JwtUserDetailsService.java
+++ src/main/java/com/albersheim/service/JwtUserDetailsService.java
@@ -36,8 +36,8 @@ public class JwtUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		if ("javainuse".equals(username)) {
-			return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+		if ("albersheim".equals(username)) {
+			return new User("albersheim", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
 					new ArrayList<>());
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
